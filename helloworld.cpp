f40d3ed4ee5a2cdc87be5cc30f9fc3a389ac1b79
Aaron Cheney <acheney@usc.edu>
2017-01-10 14:12:39 -0800

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,6 +6,7 @@ int main(int argc, char* argv[])
 	std::cout << "CS 104 rules!" << std::endl;
 	std::cout << "CS 104 rules!" << std::endl;
 	std::cout << "CS 104 rules!" << std::endl;
+	std::cout << "CS 104 rules!" << std::endl;
 
 	return 0;
 }
