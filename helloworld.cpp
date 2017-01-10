f9eb3fea13e14069999d5e29b16de5b30f354f2b
Aaron Cheney <acheney@usc.edu>
2017-01-10 14:00:43 -0800

    Adding third line

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,8 @@ int main(int argc, char* argv[])
 {
 	std::cout << "Hello world!" << std::endl;
 	std::cout << "CS 104 rules!" << std::endl;
+	std::cout << "CS 104 rules!" << std::endl;
+
 
 	return 0;
 }
